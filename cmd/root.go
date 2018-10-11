78f1470ab03fee62df31925f196d5e41b4151e91
Ryan King <ryan@theryanking.com>
2018-10-11 10:10:23 -0700

    public acl support (#51)
    public acl supportI think we'll stick to this simple level of support for now, then figure out later if we can do something deeper.
    
    Fixes #33

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	debug bool
+	quiet bool
+)
+
 func init() {
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "enable verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "do not output to console; use return code to determine success/failure")
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "reaper",
 	Short: "",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		logLevel := log.InfoLevel
+		if debug { // debug overrides quiet
+			logLevel = log.DebugLevel
+		} else if quiet {
+			logLevel = log.FatalLevel
+		}
+		log.SetLevel(logLevel)
+		log.Infof("log level set to %s", log.GetLevel())
+		return nil
+	},
 }
 
 // Execute executes the command
