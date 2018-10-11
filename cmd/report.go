d75b6b71ff4acd6e28b73ab61778575e9a8476e4
Ryan King <rking@chanzuckerberg.com>
2018-10-11 09:58:04 -0700

    DRY log level setting

--- cmd/report.go
+++ cmd/report.go
@@ -27,14 +27,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		logLevel := log.InfoLevel
-		if debug { // debug overrides quiet
-			logLevel = log.DebugLevel
-		} else if quiet {
-			logLevel = log.FatalLevel
-		}
-		log.SetLevel(logLevel)
-
 		conf, err := getConfig(cmd)
 		if err != nil {
 			return errors.Wrap(err, "could not read config")
--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,17 @@ func init() {
 var rootCmd = &cobra.Command{
 	Use:   "reaper",
 	Short: "",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		logLevel := log.InfoLevel
+		if debug { // debug overrides quiet
+			logLevel = log.DebugLevel
+		} else if quiet {
+			logLevel = log.FatalLevel
+		}
+		log.SetLevel(logLevel)
+		log.Infof("log level set to %s", log.GetLevel())
+		return nil
+	},
 }
 
 // Execute executes the command
